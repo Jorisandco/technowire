e85320d3df19e861b4805fac4590c748cbe9091a
jorisvanrijn2006@gmail.com <jorisvanrijn2006@gmail.com>
2023-09-27 10:22:42 +0200

    basic technowire backup
    uploaded basic technowire project to git.hub

---
+++ JavaScript/basic javascript.js
@@ -0,0 +1,8 @@
+function hidebar() {
+    var x = document.getElementById("linkbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
